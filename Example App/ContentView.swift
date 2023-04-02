a38fe7fb547dcf20b3f7680dde183d18c2d82e5d
tokiya <tiki1091@outlook.jp>
2023-04-02 20:39:35 +0900

    Initial Commit

---
+++ Example App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Example App
+//
+//  Created by TOKIYA on 2023/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example App/Example_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Example_AppApp.swift
+//  Example App
+//
+//  Created by TOKIYA on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct Example_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
